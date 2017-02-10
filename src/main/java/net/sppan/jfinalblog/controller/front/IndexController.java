acd34f38abd8adedef2373999c567289d98e8918
whoismy8023 <whoismy8023@163.com>
2017-02-10 19:11:48 +0800

    fix bug

--- src/main/java/net/sppan/jfinalblog/controller/front/IndexController.java
+++ src/main/java/net/sppan/jfinalblog/controller/front/IndexController.java
@@ -5,7 +5,7 @@ import net.sppan.jfinalblog.controller.BaseController;
 public class IndexController extends BaseController {
 
 	public void index(){
-		redirect("/c");
+		redirect("/c/0");
 	}
 	
 }
