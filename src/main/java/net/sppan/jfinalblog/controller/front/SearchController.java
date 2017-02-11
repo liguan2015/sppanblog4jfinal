c466fc588ab172e5b80ee41f81483238314f9fee
whoismy8023 <whoismy8023@163.com>
2017-02-11 23:45:46 +0800

    搜索功能关键字高亮

--- src/main/java/net/sppan/jfinalblog/controller/front/SearchController.java
+++ src/main/java/net/sppan/jfinalblog/controller/front/SearchController.java
@@ -13,6 +13,16 @@ public class SearchController extends BaseController{
 		String keyWord = getPara("keyWord");
 		int pageNumber = getParaToInt("p",1);
 		Page<Record> page = blogService.getPageNoContentSearch(pageNumber,5,keyWord);
+		//处理关键字高亮
+		for (Record record : page.getList()) {
+			String title = record.get("title");
+			title = title.replaceAll(keyWord, "<font color='red'>"+keyWord+"</font>");
+			record.set("title", title);
+			
+			String summary = record.get("summary");
+			summary = summary.replaceAll(keyWord, "<font color='red'>"+keyWord+"</font>");
+			record.set("summary", summary);
+		}
 		setAttr("blogPage", page);
 		setAttr("keyWord", keyWord);
 		render("index.html");
