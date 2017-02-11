7f635d1025061ce622530db9a91648bcea8f63ca
whoismy8023 <whoismy8023@163.com>
2017-02-11 21:21:34 +0800

    代码结构修改

--- src/main/java/net/sppan/jfinalblog/controller/front/BlogController.java
+++ src/main/java/net/sppan/jfinalblog/controller/front/BlogController.java
@@ -1,5 +1,8 @@
 package net.sppan.jfinalblog.controller.front;
 
+import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.plugin.activerecord.Record;
+
 import net.sppan.jfinalblog.controller.BaseController;
 import net.sppan.jfinalblog.model.Blog;
 import net.sppan.jfinalblog.service.BlogService;
@@ -8,6 +11,15 @@ public class BlogController extends BaseController{
 	private final BlogService blogService = BlogService.me;
 	
 	public void index(){
+		Integer categoryId = getParaToInt() == null ? 0 :getParaToInt();
+		int pageNumber = getParaToInt("p",1);
+		Page<Record> page = blogService.getPageNoContent(pageNumber,5,categoryId,false);
+		setAttr("blogPage", page);
+		setAttr("c", categoryId);
+		render("index.html");
+	}
+	
+	public void view(){
 		Integer blogId = getParaToInt();
 		Blog blog = blogService.findFullById(blogId);
 		setAttr("blog", blog);
--- src/main/java/net/sppan/jfinalblog/controller/front/IndexController.java
+++ src/main/java/net/sppan/jfinalblog/controller/front/IndexController.java
@@ -1,26 +1,10 @@
 package net.sppan.jfinalblog.controller.front;
 
-import com.jfinal.plugin.activerecord.Page;
-import com.jfinal.plugin.activerecord.Record;
-
 import net.sppan.jfinalblog.controller.BaseController;
-import net.sppan.jfinalblog.service.BlogService;
-import net.sppan.jfinalblog.service.CategoryService;
 
 public class IndexController extends BaseController {
-	private final BlogService blogService = BlogService.me;
-	private final CategoryService categoryService = CategoryService.me;
 
 	public void index(){
-		Integer categoryId = getParaToInt();
-		//如果没有指定分类，则使用首页
-		if(categoryId == null){
-			categoryId = 0;
-		}
-		int pageNumber = getParaToInt("p",1);
-		Page<Record> page = blogService.getPageNoContent(pageNumber,5,categoryId,false);
-		setAttr("blogPage", page);
-		setAttr("c", categoryService.findById(categoryId));
-		render("index.html");
+		redirect("/b/0");
 	}
 }
