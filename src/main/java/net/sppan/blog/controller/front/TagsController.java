45de265f249dbd2f994800a84ca990bb9cc6f18d
whoismy8023 <whoismy8023@163.com>
2017-04-26 11:09:36 +0800

    处理标签为中文名字的时候出现url编码的问题

--- src/main/java/net/sppan/blog/controller/front/TagsController.java
+++ src/main/java/net/sppan/blog/controller/front/TagsController.java
@@ -1,5 +1,8 @@
 package net.sppan.blog.controller.front;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import com.jfinal.core.Controller;
 import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Record;
@@ -12,6 +15,11 @@ public class TagsController extends Controller{
 	public void index(){
 		int pageNumber = getParaToInt("p",1);
 		String tagName = getPara();
+		try {
+			tagName = URLDecoder.decode(tagName,"UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 		Page<Record> page = blogService.findPageNoContentTag(pageNumber, 5, tagName);
 		setAttr("blogPage", page);
 		setAttr("tagName", tagName);
